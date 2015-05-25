97297ffd7178021ee851b1f72c86141082056643
ruby_007 <793326183@qq.com>
2015-05-25 11:20:24 +0800

    bug

--- db/migrate/20150427020754_remove_categories_id_from_article.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCategoriesIdFromArticle < ActiveRecord::Migration
-  def change
-    remove_column :articles, :categories_id, :string
-  end
-end
--- db/migrate/20150518013804_add_hot_to_article.rb
+++
@@ -1,5 +0,0 @@
-class AddHotToArticle < ActiveRecord::Migration
-  def change
-    add_column :articles, :hot, :string
-  end
-end
